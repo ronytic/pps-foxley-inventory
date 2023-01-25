b5a58702a2085595e8bcd77a41ab2c5ed89b6277
Ronald Franz Nina Layme <ronald.nina.layme@gmail.com>
2023-01-25 17:42:21 -0400

    Update AddToMenus.php

--- src/Http/Middleware/AddToMenus.php
+++ src/Http/Middleware/AddToMenus.php
@@ -1,4 +1,5 @@
 <?php
+
 namespace ProcessMaker\Package\PpsFoxleyInventory\Http\Middleware;
 
 use Closure;
@@ -14,17 +15,16 @@ class AddToMenus
         // Add a menu option to the top to point to our page
 
         $menu = Menu::get('topnav');
-        $menu->add(__('Skeleton'), ['route' => 'package.skeleton.tab.index']);
+        $menu->add(__('Foxley inventory'), ['route' => 'package.skeleton.tab.index']);
 
         // Add a option in the admin menu to point to our page
         $menu = Menu::get('sidebar_admin')->first();
 
         // Add our menu item to the top nav
-        $menu->add(__('Skeleton'), [
+        $menu->add(__('Foxley inventory 2'), [
             'route' => 'package.skeleton.index',
             'icon' => 'fa-puzzle-piece',
         ]);
         return $next($request);
     }
-
 }
