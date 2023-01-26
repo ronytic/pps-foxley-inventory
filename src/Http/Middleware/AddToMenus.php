f7cba731232451d54c869ead218f23777d38e9fe
Ronald Franz Nina Layme <ronald.nina.layme@gmail.com>
2023-01-26 12:52:45 -0400

    Update AddToMenus.php

--- src/Http/Middleware/AddToMenus.php
+++ src/Http/Middleware/AddToMenus.php
@@ -15,13 +15,13 @@ class AddToMenus
         // Add a menu option to the top to point to our page
 
         $menu = Menu::get('topnav');
-        $menu->add(__('Foxley inventory 3'), ['route' => 'package.foxleyinventory.tab.index']);
+        $menu->add(__('Foxley inventory Luchito'), ['route' => 'package.foxleyinventory.tab.index']);
 
         // Add a option in the admin menu to point to our page
         $menu = Menu::get('sidebar_admin')->first();
 
         // Add our menu item to the top nav
-        $menu->add(__('Foxley inventory 3'), [
+        $menu->add(__('Foxley inventory Luchito'), [
             'route' => 'package.foxleyinventory.index',
             'icon' => 'fa-users',
         ]);
